c56c10fd04474bc1430ae638ef0a20d70259a6ab
Yutaka Nagashima <staccato.jp@gmail.com>
2013-04-18 20:39:02 +0900

    HomeWork 2.1

--- HomeWorks/2.1/ModalFromTab/ModalFromTab/MFTAppDelegate.m
+++ HomeWorks/2.1/ModalFromTab/ModalFromTab/MFTAppDelegate.m
@@ -24,7 +24,8 @@
     self.tabBarController.viewControllers = @[viewController1, viewController2];
 
     // TODO : tabBarController の delegateを自分にセット
-
+    self.tabBarController.delegate = self;
+    
     self.window.rootViewController = self.tabBarController;
     [self.window makeKeyAndVisible];
     return YES;
@@ -71,16 +72,18 @@
 }
 */
 
-
-/*
 // TODO
 // コメントアウトをはずすと、タブがタップされた時にこのメソッドが呼ばれます
 // YESを返すとタブが選択され、NOを返すと選択されません
 // モーダルを出したい時はモーダルを表示してNOを返してください
 - (BOOL)tabBarController:(UITabBarController *)tabBarController shouldSelectViewController:(UIViewController *)viewController
 {
-
+    if ([viewController.nibName isEqual: @"MFTSecondViewController"]) {
+        MFTModalViewController *modalViewController = [[MFTModalViewController alloc]init];
+        [viewController presentViewController:modalViewController animated:YES completion:nil];
+        return NO;
+    }
+    return YES;
 }
-*/
 
 @end
